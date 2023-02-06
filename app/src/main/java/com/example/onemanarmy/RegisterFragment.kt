302630cec817712b2ca01f789ce7ac67c9f0eb10
ayarbray <yarbraya18@students.ecu.edu>
2023-02-06 18:02:10 -0500

    Login and Registration page functional with errors

--- app/src/main/java/com/example/onemanarmy/RegisterFragment.kt
+++ app/src/main/java/com/example/onemanarmy/RegisterFragment.kt
@@ -1,11 +1,16 @@
 package com.example.onemanarmy
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.ButtonBarLayout
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +23,10 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class RegisterFragment : Fragment() {
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var cnfPassword: EditText
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,13 +46,64 @@ class RegisterFragment : Fragment() {
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_register, container, false)
 
+        username = view.findViewById(R.id.reg_username)
+        password = view.findViewById(R.id.reg_password)
+        cnfPassword = view.findViewById(R.id.reg_cnf_password)
+
+
         view.findViewById<Button>(R.id.btn_login_reg).setOnClickListener {
             var navRegister = activity as FragmentNavigation
             navRegister.navigateFrag(LoginFragment(), false)
 
         }
+        view.findViewById<Button>(R.id.btn_register_reg).setOnClickListener {
+            validateEmptyForm()
+        }
+
         return view
 
+    }
+
+    private fun validateEmptyForm(){
+        val icon = AppCompatResources.getDrawable(requireContext(),
+        R.drawable.errorsymbol)
+
+        icon?.setBounds(0,0, icon.intrinsicWidth,icon.intrinsicHeight)
+        when{
+            TextUtils.isEmpty(username.text.toString().trim())->{
+                username.setError("Please Enter Username",icon)
+            }
+            TextUtils.isEmpty(password.text.toString().trim())->{
+                password.setError("Please Enter Password",icon)
+            }
+            TextUtils.isEmpty(cnfPassword.text.toString().trim())->{
+                cnfPassword.setError("Please Enter Password Again",icon)
+            }
+
+            username.text.toString().isNotEmpty() &&
+                    password.text.toString().isNotEmpty() &&
+                    cnfPassword.text.toString().isNotEmpty() ->
+            {
+                if(username.text.toString().matches(Regex("123456"))){
+                 if(password.text.toString().length>=5) {
+
+                     if(password.text.toString() == cnfPassword.text.toString()){
+
+                         Toast.makeText(context,"Registration Successful", Toast.LENGTH_SHORT).show()
+                     }
+                     else{
+                         cnfPassword.setError("Password didn't match", icon)
+                     }
+                 }
+                  else{
+                      password.setError("Please Enter at least 5 characters",icon)
+                  }
+
+                }else{
+                    username.setError("Please Enter Valid Email Id",icon)
+                }
+            }
+        }
     }
 
     companion object {
