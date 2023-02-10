ea4dd3b4798a234d3e4f7395068eddba433e3aeb
eliashazboun <hazboune14@students.ecu.edu>
2023-02-10 18:48:44 -0500

    Owner Dashboard XML finished.

---
+++ app/src/main/java/com/example/onemanarmy/OwnerDashboard.kt
@@ -0,0 +1,23 @@
+package com.example.onemanarmy
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.onemanarmy.databinding.ActivityOwnerDashboardBinding
+
+class OwnerDashboard : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.owner_dashboard)
+
+        }
+    }
+
+
