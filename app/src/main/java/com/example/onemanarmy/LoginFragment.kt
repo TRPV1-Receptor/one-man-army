80a30965f896179d0296298d5f7e92ae09de667c
Anthony Miranda <mirandaj20@students.ecu.edu>
2023-02-24 11:37:44 -0500

    Login Authentication Finalized
    Registered accounts should be able to login and will then be redirected to owner dashboard upon logging in.

--- app/src/main/java/com/example/onemanarmy/LoginFragment.kt
+++ app/src/main/java/com/example/onemanarmy/LoginFragment.kt
@@ -12,6 +12,7 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +26,7 @@ import androidx.navigation.fragment.findNavController
 class LoginFragment : Fragment() {
     private lateinit var username: EditText
     private lateinit var password: EditText
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,6 +38,7 @@ class LoginFragment : Fragment() {
 
         username = view.findViewById(R.id.log_username)
         password = view.findViewById(R.id.log_password)
+        firebaseAuth = FirebaseAuth.getInstance()
 
         view.findViewById<Button>(R.id.btn_register).setOnClickListener {
             var navRegister = activity as FragmentNavigation
@@ -86,7 +89,7 @@ class LoginFragment : Fragment() {
                     password.text.toString().isNotEmpty() ->
             {
                 if(username.text.toString().matches(Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"))){
-
+                firebaseSignIn()
                 Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
 
                     //The commented out lines are causing the text in the username box to
@@ -97,6 +100,18 @@ class LoginFragment : Fragment() {
                 }
             }
         }
+
+    private fun firebaseSignIn() {
+        firebaseAuth.signInWithEmailAndPassword(username.text.toString(), password.text.toString()).addOnCompleteListener{
+            if (it.isSuccessful) {
+                val intent = Intent(context, OwnerDashboard::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(context, it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
     }
 
 
