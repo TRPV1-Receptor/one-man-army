2bc4163e64e23ca3b8898085da0e793c916bca45
Deidre240 <deidrewhitehead000@gmail.com>
2023-03-22 23:35:38 -0400

    For settings page and Account Page

--- app/src/main/java/com/example/onemanarmy/AccountActivity.kt
+++ app/src/main/java/com/example/onemanarmy/AccountActivity.kt
@@ -2,10 +2,26 @@ package com.example.onemanarmy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class AccountActivity : AppCompatActivity() {
+
+    private lateinit var saveData: SaveData
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //share preference
+        saveData = SaveData(this)
+        if (saveData.loadDarkState() == true) {
+            setTheme(R.style.darkTheme)
+        } else
+            setTheme(R.style.AppTheme)
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
+
+
+        val backBtn = findViewById<ImageView>(R.id.back)
+        backBtn.setOnClickListener { finish() }
     }
 }
