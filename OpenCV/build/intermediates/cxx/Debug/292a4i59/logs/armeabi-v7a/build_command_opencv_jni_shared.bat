9084b8697349c3730ec6e7ad660506fd498d2c62
eliashazboun <hazboune14@students.ecu.edu>
2023-03-29 14:15:46 -0400

    OpenCV and ScanLibrary

---
+++ OpenCV/build/intermediates/cxx/Debug/292a4i59/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Captain Hook\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Captain Hook\\Desktop\\New folder\\oneManArmy\\OpenCV\\.cxx\\Debug\\292a4i59\\armeabi-v7a" ^
+  opencv_jni_shared
---
+++ OpenCV/build/intermediates/cxx/Debug/292a4i59/logs/x86_64/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\Captain Hook\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\Captain Hook\\Desktop\\New folder\\oneManArmy\\OpenCV\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=23" ^
+  "-DANDROID_PLATFORM=android-23" ^
+  "-DANDROID_ABI=x86_64" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86_64" ^
+  "-DANDROID_NDK=C:\\Users\\Captain Hook\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\Captain Hook\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\Captain Hook\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\Captain Hook\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\Captain Hook\\Desktop\\New folder\\oneManArmy\\OpenCV\\build\\intermediates\\cxx\\Debug\\292a4i59\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\Captain Hook\\Desktop\\New folder\\oneManArmy\\OpenCV\\build\\intermediates\\cxx\\Debug\\292a4i59\\obj\\x86_64" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\Captain Hook\\Desktop\\New folder\\oneManArmy\\OpenCV\\.cxx\\Debug\\292a4i59\\x86_64" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
