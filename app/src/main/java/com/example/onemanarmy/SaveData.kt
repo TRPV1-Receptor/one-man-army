9846f0e678890670192799c8f0c962c89dc90a3e
eliashazboun <hazboune14@students.ecu.edu>
2023-04-03 10:04:10 -0400

    Test

--- app/src/main/java/com/example/onemanarmy/SaveData.kt
+++ app/src/main/java/com/example/onemanarmy/SaveData.kt
@@ -3,7 +3,7 @@ package com.example.onemanarmy
 import android.content.Context
 import android.content.SharedPreferences
 
-class SaveData(context:Context) {
+class   SaveData(context:Context) {
 
     private var sharedPreferences: SharedPreferences = context.getSharedPreferences("file", Context.MODE_PRIVATE)
 
