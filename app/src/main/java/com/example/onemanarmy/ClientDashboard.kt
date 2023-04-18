8db8ad4030616c1c7ccd722ec4d558336db8e2a3
eliashazboun <hazboune14@students.ecu.edu>
2023-04-18 17:25:57 -0400

    Set up receipt creator as clickable to the search

--- app/src/main/java/com/example/onemanarmy/ClientDashboard.kt
+++ app/src/main/java/com/example/onemanarmy/ClientDashboard.kt
@@ -1,11 +1,19 @@
 package com.example.onemanarmy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class ClientDashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_dashboard)
+
+        val searchButton = findViewById<ConstraintLayout>(R.id.search)
+        searchButton.setOnClickListener{
+            val intent = Intent(this,SearchActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
